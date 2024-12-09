c20725105b32d4016dd85f80ab8a5c54fa41f5fc
hansemin <han299792@gmail.com>
2024-12-09 20:25:28 +0900

    param에 ParseIntPipe 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Get,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from '@prisma/client';
+import { User, UserPreference } from '@prisma/client';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/createUser.dto';
 
@@ -23,7 +32,7 @@ export class UserController {
   @ApiResponse({ status: 200, description: 'User successfully updated.' })
   @ApiResponse({ status: 404, description: 'User not found.' })
   async updateUser(
-    @Param('id') userId: number,
+    @Param('id', ParseIntPipe) userId: number,
     @Body() updateData: Partial<User>,
   ): Promise<User> {
     return this.userService.updateUser(userId, updateData);
@@ -33,7 +42,49 @@ export class UserController {
   @ApiOperation({ summary: 'Delete a user' })
   @ApiResponse({ status: 200, description: 'User successfully deleted.' })
   @ApiResponse({ status: 404, description: 'User not found.' })
-  async deleteUser(@Param('id') userId: number): Promise<void> {
+  async deleteUser(@Param('id', ParseIntPipe) userId: number): Promise<void> {
     return this.userService.deleteUser(userId);
+  }
+
+  @Get('info/:id')
+  @ApiOperation({ summary: 'Get user information with preferences' })
+  @ApiResponse({
+    status: 200,
+    description: 'User information retrieved successfully.',
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  async getUserInfo(@Param('id', ParseIntPipe) userId: number) {
+    return this.userService.getUserInfo(userId);
+  }
+
+  @Post('preference/:id')
+  @ApiOperation({ summary: 'Save or update user preferences' })
+  @ApiResponse({
+    status: 201,
+    description: 'User preferences saved successfully.',
+    schema: {
+      example: {
+        user_id: 1,
+        is_user_halal: true,
+        is_user_vegan: false,
+        is_user_peanut: true,
+      },
+    },
+  })
+  @ApiResponse({ status: 400, description: 'Invalid request payload.' })
+  @ApiBody({
+    schema: {
+      example: {
+        is_user_halal: true,
+        is_user_vegan: false,
+        is_user_peanut: true,
+      },
+    },
+  })
+  async saveUserPreference(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() data: Partial<UserPreference>,
+  ) {
+    return this.userService.saveUserPreference(userId, data);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository'; // Repository 파일을 import
 import { CreateUserDto } from './dto/createUser.dto';
-import { User } from '@prisma/client';
+import { User, UserPreference } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -17,5 +17,11 @@ export class UserService {
 
   async deleteUser(userId: number): Promise<void> {
     return this.userRepository.deleteUser(userId);
+  }
+  async getUserInfo(userId: number) {
+    return this.userRepository.getUserWithPreferences(userId);
+  }
+  async saveUserPreference(userId: number, data: Partial<UserPreference>) {
+    return this.userRepository.createOrUpdateUserPreference(userId, data);
   }
 }
